e813922a1ed723c2b6a837854f57cb4b41d8ace2
Praveen Yerneni <yernenip@gmail.com>
2020-07-04 20:47:25 -0700

    Updated code and fixed the issue with storing incorrect palindrome number

--- LargestPalindrome.py
+++ LargestPalindrome.py
@@ -1,4 +1,12 @@
 
+# This python code determines the largest palindrome number as a result of 2,3 or 4 digit numbers.
+# For example for a 2 digit number the largest palindrome is 9009 and the digits are 91 and 99
+# The first part of the code accepts the number of digits you would want to check with.
+# The second part then iterates over that many number of digits starting from lowest value to highest
+# It uses a nested loop so complexity is O(n^2)
+
+
+#function to check if a given number is a palindrome
 def ispalindrome(outputnum):
     return outputnum == outputnum[::-1]
 
@@ -8,25 +16,30 @@ print("Welcome! Find the largest paindrome number made from the product of two n
 numberofdigits = 0
 palindromenumber = 0
 
+# Accepts numbers greater than 2 digits and less than 4 digits. Any digits beyond 4 will cause performance.
 while numberofdigits < 2 or numberofdigits > 4:
     numberofdigits = int(input("Please enter the number of digits for your two numbers (upto 4 digits): "))
 
 seed = 0
 
+# set the starting value for the number of digits
 while len(str(seed)) < numberofdigits:
     seed = seed + 1
 
+firstnumber = seed
 
-while len(str(seed)) < numberofdigits + 1:
+# Loop through the numbers, incrementing the second number and multiplying with first.
+while len(str(firstnumber)) < numberofdigits + 1:
     secondnumber = seed
     while len(str(secondnumber)) < numberofdigits + 1:
-        multiple = seed * secondnumber
+        multiple = firstnumber * secondnumber
         if ispalindrome(str(multiple)):
-            palindromenumber = multiple
-            numberone = seed
-            numbertwo = secondnumber
+            if multiple > palindromenumber:
+                palindromenumber = multiple
+                numberone = firstnumber
+                numbertwo = secondnumber
         secondnumber = secondnumber + 1
-    seed = seed + 1
+    firstnumber = firstnumber + 1
 
 
 print(f"The largest palindrome number for {numberofdigits} digits is {palindromenumber}")
